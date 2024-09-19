c6cef4f63208e0c15c146f37081cc54c19882045
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-19 17:33:09 +0300

    Initial Commit

---
+++ MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Beyza Nur Tekerek on 19.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
